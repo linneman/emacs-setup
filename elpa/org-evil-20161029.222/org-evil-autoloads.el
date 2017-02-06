278e98c744949224b8bb67acb7c6d310c05d277f
Otto Linnemann <linneman@gmx.de>
2017-02-07 00:12:35 +0100

    add org-evil mode, org-bullets and multi-term
    Signed-off-by: Otto Linnemann <linneman@gmx.de>

---
+++ elpa/org-evil-20161029.222/org-evil-autoloads.el
@@ -0,0 +1,17 @@
+;;; org-evil-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("org-evil-commands.el" "org-evil-core.el"
+;;;;;;  "org-evil-motion.el" "org-evil-pkg.el" "org-evil-table.el"
+;;;;;;  "org-evil.el") (22680 49970 303666 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; org-evil-autoloads.el ends here
