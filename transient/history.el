12b3cc8d21e71528201c431c39305351900c238f
Otto Linnemann <linneman@gmx.de>
2023-01-05 21:41:07 +0100

    update cider to v1.6 ("Buenos Aires")
    Signed-off-by: Otto Linnemann <linneman@gmx.de>

--- transient/history.el
+++ transient/history.el
@@ -1,2 +1,9 @@
 ((magit-commit nil)
+ (magit-diff
+  ("--no-ext-diff" "--stat"))
+ (magit-dispatch nil)
+ (magit-log
+  ("-n256" "--graph" "--decorate"))
+ (magit-rebase
+  ("--autostash"))
  (magit-status-jump nil))
