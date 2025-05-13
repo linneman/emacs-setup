744f2ff991019a50db43f1d5996ce437453613b7
Otto Linnemann <otto.linnemann@valeo.com>
2025-05-13 08:53:18 +0200

    add geiser, dired-quick-sort and dependencies
    Signed-off-by: Otto Linnemann <otto.linnemann@valeo.com>

---
+++ elpa/geiser-chibi-20240521.2252/geiser-chibi-pkg.el
@@ -0,0 +1,11 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "geiser-chibi" "20240521.2252"
+  "Chibi Scheme's implementation of the geiser protocols."
+  '((emacs  "24.4")
+    (geiser "0.18"))
+  :url "https://gitlab.com/emacs-geiser/chibi"
+  :commit "2502fed1349c2703eea528b74bcc980ad6bceab8"
+  :revdesc "2502fed1349c"
+  :keywords '("languages" "chibi" "scheme" "geiser")
+  :authors '(("Peter" . "craven@gmx.net"))
+  :maintainers '(("Jose A Ortega Ruiz" . "jao@gnu.org")))
